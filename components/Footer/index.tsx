51172bf80e0dc70493a072b7b72ee3e8784860d5
RajKunx2 <113442064+RajKunx2@users.noreply.github.com>
2023-06-03 12:43:37 +0530

    Footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,11 +1,68 @@
-import React from 'react'
+import React from 'react';
 
-type Props = {}
-
-const index = (props: Props) => {
+const Footer = () => {
   return (
-    <div className='bg-white text-black'>footer here</div>
-  )
-}
+    <footer className="relative bg-blueGray-200 pt-8 pb-6">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-wrap text-left lg:text-left">
+          <div className="w-full lg:w-6/12 px-4">
+            <h1 className='special-font text-[3.7rem] text-4xl bg-white bg-clip-text text-primary'>PASTRY EVENT</h1>
+            <h5 className="text-lg mt-0 mb-2 text-blueGray-600">
+              Sweeten your senses, indulge in mastery!
+              Join us for a delectable pastry showdown!
+            </h5>
+          </div>
+          <div className="w-full lg:w-6/12 px-4">
+            <div className="flex flex-wrap items-top mb-6">
+              <div className="w-full lg:w-4/12 px-4 ml-auto">
+                <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Useful Links</span>
+                <ul className="list-unstyled">
+                  <li>
+                    <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm">About Us</a>
+                  </li>
+                  <li>
+                    <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm">Participants</a>
+                  </li>
+                  <li>
+                    <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm">Gallery</a>
+                  </li>
+                  <li>
+                    <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="/JPIC Guidelines & Rules.pdf">Rules And Regulations</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="w-full lg:w-4/12 px-4">
+                <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Other Resources</span>
+                <ul className="list-unstyled">
+                  <li>
+                    <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm">MIT License</a>
+                  </li>
+                  <li>
+                    <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm">Terms &amp; Conditions</a>
+                  </li>
+                  <li>
+                    <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm">Privacy Policy</a>
+                  </li>
+                  <li>
+                    <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm">Contact Us</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr className="my-6 border-blueGray-300" />
+        <div className="flex flex-wrap items-center md:justify-between justify-center">
+          <div className="w-full md:w-4/12 px-4 mx-auto text-center">
+            <div className="text-sm text-blueGray-500 font-semibold py-1">
+              Copyright © <span id="get-current-year">Junior Pastry Indian Cup 2023</span>
 
-export default index
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- components/Sponsors/index.jsx
+++ components/Sponsors/index.jsx
@@ -11,7 +11,7 @@ export default function Sponsors() {
     return (
         <div className='bg-white text-black flex items-center flex-col'>
             <h1 className='font-ProzaLibre text-5xl md:text-7xl text-pink-500'>SPONSORS</h1>
-            <div className={`flex flex-col lg:flex-row bg-pink-300/80 shadow-2xl backdrop-blur-3xl rounded-lg px-4 py-6 items-center justify-center w-fit m-4 ${showModal1 ? "h-screen" : ""}`}>
+            <div className={`flex flex-col lg:flex-row bg-pink-300/80 shadow-2xl backdrop-blur-3xl rounded-lg px-4 py-6 items-center justify-center w-fit m-4 ${showModal1 ? "md:h-screen h-fit" : ""}`}>
                 <div className='rounded-lg mx-4'>
                     <Image alt='' src={Trial} width={180} height={180} />
                 </div>
@@ -19,11 +19,11 @@ export default function Sponsors() {
                     <div className='text-3xl text-pink-600 font-Lustria font-semibold lg:mb-2 my-3 lg:my-0 mx-auto lg:mx-0'>QUINCY</div>
                     <div className='text-lg font-Quattrocento'>
                         Calling all pastry enthusiasts! The ultimate pastry competition awaits you! Show off your culinary skills and create mouthwatering masterpieces that will leave taste buds dancing. From delectable cakes to delicate pastries, bring your A-game and win the title of Pastry Champion. Join us on [date] at [venue] and let your creativity shine. Register now and let the baking battle begin
-                        <button className='text-white font-bold ml-2' onClick={() => setShowModal1(true)}>Read More</button>
+                        <button className='text-white font-semibold ml-2' onClick={() => setShowModal1(true)}>Read More</button>
                         {showModal1 ? (
                             <>
                                 <div
-                                    className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                                    className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none mt-4"
                                 >
                                     <div className="relative my-6 mx-auto w-fit">
                                         {/*content*/}
@@ -41,24 +41,22 @@ export default function Sponsors() {
                                             </div>
                                             {/*body*/}
                                             <div className="relative p-6 flex-auto">
-                                                <p className="my-4 text-slate-500 text-base leading-relaxed">
-                                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis nihil dolor eaque, obcaecati eos temporibus est facere cum natus quos, autem sit quasi saepe error assumenda architecto recusandae porro repellat!
-                                                    Aliquid libero enim repellendus deleniti iste. Eius perspiciatis iure aliquid nam ad, repellendus minima praesentium id dolore excepturi et tenetur expedita libero magnam possimus ut voluptate quisquam dicta perferendis animi!
-                                                    Dolorem impedit dignissimos et rerum cum magni ipsa autem repellat sint fugit, recusandae pariatur eaque dolores. Repellat recusandae, odio amet magni vel ullam quis in ipsa, inventore iure at magnam.
-                                                    Deleniti repellendus, eaque facere consequatur adipisci enim reiciendis aliquam inventore iusto eius possimus, fuga dolore nihil dolores doloribus ipsa assumenda commodi atque, voluptatibus id ratione. Ullam reiciendis recusandae dolores fugit!
-                                                    Dignissimos fugiat quae ex, possimus corrupti ut temporibus voluptas quia, recusandae dolorem repellendus! Suscipit voluptas ea libero, culpa fugit tempore nemo odit sit voluptate illum, reprehenderit quasi sunt corrupti voluptatibus!
-                                                    Perferendis reprehenderit accusantium illum quis animi libero sint? Fugit adipisci sunt dolores porro, quis sequi, corporis iste suscipit labore deleniti voluptatem pariatur illo modi odit dolorem error? Distinctio, ipsam accusamus!
-                                                    Facilis sapiente sit exercitationem doloremque beatae excepturi placeat laboriosam, tempore, unde aperiam eum, laudantium repellat minima nisi distinctio neque! Velit eveniet maxime debitis eius rerum laboriosam at qui minima modi?
-                                                    Ratione vero eaque quisquam velit a aperiam delectus? Nihil soluta laboriosam magnam eveniet similique inventore deserunt accusantium ipsam! Architecto ab adipisci commodi sunt consequuntur a placeat illo voluptatem alias nostrum!
-                                                    Cupiditate nisi amet possimus? Aperiam optio temporibus obcaecati quidem, voluptatum dolorum ipsa totam voluptatem, repudiandae quos consequatur ex error, voluptatibus architecto quasi nulla similique dicta ullam recusandae esse. Non, architecto!
-                                                    Distinctio dolor voluptate cumque voluptates, veniam, consequuntur saepe velit laborum assumenda molestiae maxime eum quod molestias suscipit libero et enim dignissimos modi odio? Repudiandae delectus sint exercitationem ullam quod veniam.
-                                                    Assumenda suscipit fuga eaque laudantium maiores. Repellendus alias maiores ipsam incidunt eveniet ratione cupiditate ut deserunt iure consectetur, vitae, placeat corrupti cumque veritatis asperiores doloremque quae atque accusantium voluptate enim?
-                                                    Repudiandae reiciendis expedita porro quaerat ut labore quidem nesciunt possimus recusandae ullam! Ratione ducimus excepturi et eveniet qui nisi facere, quasi soluta, iste amet nostrum quaerat voluptatem impedit? Ullam, nihil?
-                                                    Quisquam placeat ab itaque laudantium dolorem sit, ratione illum iusto dicta exercitationem quibusdam, alias nihil odit. Sint, similique? Aliquid ipsa repudiandae odit quam, consequatur sapiente quas nemo adipisci architecto quaerat.
-                                                    Consequuntur corporis explicabo reprehenderit ullam ipsa quia laboriosam? Beatae adipisci ipsam veniam! Iste repellat quibusdam dolore provident deleniti consequuntur, non, porro quaerat delectus, sed natus hic magni. Quasi, quam quas.
-                                                    Quia nesciunt soluta, dolorum ab quisquam totam in dolores beatae, quos incidunt minus laudantium repellat? Saepe molestias est doloribus nam et quidem recusandae, at enim, magni, nisi pariatur mollitia commodi?
-                                                    Quasi explicabo mollitia aliquid corrupti commodi esse labore animi laudantium, laborum quaerat totam aut dolores reprehenderit! Amet recusandae perferendis exercitationem doloremque doloribus error voluptatem soluta? Tempora corporis temporibus consequuntur dolorem!
-                                                    Quod doloribus fugit temporibus et culpa quibusdam architecto consequatur possimus veritatis distinctio, nihil aspernatur corrupti sed odit eaque accusamus esse dolores repellat, similique facilis est! Nihil tempore est doloremque soluta!
+                                                <p className="my-4 text-slate-500 lg:text-base leading-relaxed text-sm">
+                                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita necessitatibus ipsum harum, autem vel nobis, enim, et delectus hic in cupiditate! Reiciendis placeat reprehenderit mollitia nam ut error dicta Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere cum soluta similique provident totam! Itaque magni alias incidunt repellat est, nemo possimus explicabo neque assumenda ad fugit mollitia expedita quis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde repellat eos veniam mollitia sit voluptatum, libero tempore id assumenda tenetur velit amet soluta illo consectetur adipisci. Saepe corporis a iusto!
+                                                    Atque beatae accusantium reiciendis consequatur ullam vitae dignissimos, cumque rem quibusdam incidunt molestiae nihil, quod suscipit autem quam culpa, aperiam dolorem maiores mollitia quas cum repellat pariatur enim. Explicabo, odio.
+                                                    Quisquam consequuntur, pariatur qui dolore obcaecati dolor ut sunt ipsa quia voluptates animi consectetur ad illum cum modi. Maxime accusamus consequatur maiores. Ducimus corrupti tempora dolore minus quod eveniet excepturi.
+                                                    Qui cupiditate asperiores eaque suscipit est? Corrupti est doloremque iste sapiente deleniti architecto in accusamus minus aliquam officiis sit, ducimus tenetur voluptatem consectetur reiciendis id aspernatur aperiam autem suscipit! Voluptatem?
+                                                    Earum expedita reprehenderit laudantium minima ullam architecto quae animi, numquam nemo velit adipisci fugit molestiae, dolorem inventore? Perspiciatis repellendus possimus quia laborum, quos, illo consequatur voluptatem blanditiis numquam, sed harum!
+                                                    Provident delectus, magni porro placeat sint rerum voluptas iste et laboriosam dolores necessitatibus quasi quas quam labore aut nemo minima beatae, ipsam odit, veniam sit eaque sed. Nisi, accusamus ad.
+                                                    Quod, natus. Impedit velit maiores suscipit cum, error, nesciunt quidem ratione dolore aliquam necessitatibus assumenda optio quia libero voluptas iure doloribus accusamus quod aperiam. Exercitationem, unde repellendus. Quibusdam, dicta praesentium?
+                                                    Facere, id odit. Vitae mollitia atque dolorem nostrum, nemo labore, non facere saepe quidem quaerat quasi hic praesentium ipsam, ea corporis ad earum ducimus quas dolore. Odit soluta ipsam cumque.
+                                                    Officiis doloribus obcaecati neque reiciendis est temporibus eum voluptates, nulla qui quisquam, ut et labore! Odio cum, sit impedit eaque atque eveniet, distinctio assumenda modi voluptatibus minima vel, dignissimos fuga?
+                                                    Amet nostrum exercitationem quo ea consequatur, nesciunt neque dignissimos, officia quas similique mollitia maxime! Ab eligendi nisi in voluptate mollitia nulla nostrum, dolores et aliquam sunt vitae sint, similique accusamus!
+                                                    Reiciendis cumque nemo ea vel nihil, iste eius recusandae est nobis eos excepturi odit, temporibus adipisci quod fugit ut explicabo quia illum nulla. Sint facere iste quasi maxime eaque officia!
+                                                    Nulla similique, cupiditate dignissimos aperiam eius repudiandae eveniet error ex? Quae ducimus perspiciatis minus dignissimos officiis, iste illo saepe possimus repellendus repudiandae laborum ipsam eveniet laudantium ab quam optio nostrum.
+                                                    Dolore quasi enim quo similique sed vitae assumenda consequatur nesciunt sapiente commodi expedita atque rem hic iste obcaecati fugiat ad ab, porro quidem veniam suscipit illo. Reiciendis quae corrupti molestias.
+                                                    Autem, odio nam? Error deserunt enim possimus similique quod odio tempora minima ullam, excepturi veniam porro, reprehenderit nulla quam ut dolore velit placeat laborum repudiandae corporis veritatis expedita optio minus?
+                                                    Inventore incidunt quisquam, quos voluptatem officiis suscipit aperiam, tempore, iste consequatur vero totam dicta earum saepe distinctio maiores eaque. Iure expedita hic earum fugiat officia. Provident temporibus tempora modi commodi!
                                                 </p>
                                             </div>
                                             {/*footer*/}
@@ -80,18 +78,7 @@ export default function Sponsors() {
                     </div>
                 </div>
             </div>
-            <div className={`flex flex-col ${showModal2 || showModal3 ? "lg:flex-col" : "lg:flex-row"} bg-white h-auto w-auto mx-4 my-6`}>
-                {/* <div className='flex flex-col basis-1/2 lg:flex-row bg-pink-300/80 shadow-2xl backdrop-blur-3xl rounded-lg px-4 py-6 items-center justify-center w-fit rounded-lgm m-4'>
-                    <div className='rounded-lg mx-4 w-max-[100px]'>
-                        <Image alt='' src={Trial} width={130} height={130} />
-                    </div>
-                    <div className='flex flex-col lg:justify-start justify-center mx-4'>
-                        <div className='text-xl text-pink-600 font-Lustria font-semibold lg:mb-2 my-3 lg:my-0 mx-auto lg:mx-0'>QUINCY</div>
-                        <div className='text-base font-Quattrocento'>
-                            Calling all pastry enthusiasts! The ultimate pastry competition awaits you! Show off your culinary skills and create mouthwatering masterpieces that will leave taste buds dancing.
-                        </div>
-                    </div>
-                </div> */}
+            <div className={`flex flex-col ${showModal2 || showModal3 ? "lg:flex-col" : "lg:flex-row"} bg-white h-auto w-auto mx-4 my-6 items-center`}>
                 <div className={`flex flex-col lg:flex-row bg-pink-300/80 shadow-2xl backdrop-blur-3xl rounded-lg px-4 py-6 items-center justify-center w-full rounded-lgm m-4 ${showModal2 ? "h-screen w-full " : "basis-1/2"}`}>
                     <div className='rounded-lg mx-4'>
                         <Image alt='' src={Trial} width={130} height={130} />
@@ -100,7 +87,7 @@ export default function Sponsors() {
                         <div className='text-xl text-pink-600 font-Lustria font-semibold lg:mb-2 my-3 lg:my-0 mx-auto lg:mx-0'>QUINCY</div>
                         <div className='text-base font-Quattrocento'>
                             Calling all pastry enthusiasts! The ultimate pastry competition awaits you! Show off your culinary skills and create mouthwatering masterpieces that will leave taste buds dancing.
-                            <button className='text-white font-bold ml-2' onClick={() => setShowModal2(true)}>Read More</button>
+                            <button className='text-white font-semibold ml-2' onClick={() => setShowModal2(true)}>Read More</button>
                             {showModal2 ? (
                                 <>
                                     <div
@@ -122,7 +109,7 @@ export default function Sponsors() {
                                                 </div>
                                                 {/*body*/}
                                                 <div className="relative p-6 flex-auto">
-                                                    <p className="my-4 text-slate-500 text-base leading-relaxed">
+                                                    <p className="my-4 text-slate-500 lg:text-base text-sm leading-relaxed">
                                                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus eveniet esse temporibus mollitia. Veniam illum eaque sit natus ab nisi sequi repudiandae vero adipisci, voluptatibus cum doloremque impedit tenetur! Alias!
                                                         Quia ducimus labore quasi cupiditate vel. Ipsa possimus, illo voluptate sequi inventore suscipit, delectus beatae et neque magni enim molestias rem consectetur maxime odio, saepe aut. Aut itaque laborum recusandae.
                                                         Rerum possimus laboriosam nisi, dolores, quibusdam sit dolore suscipit, ipsum voluptates sequi id et perferendis natus quasi. Numquam, autem, odit in iusto ipsa pariatur, explicabo iure ad dolorum nostrum ratione.
@@ -159,7 +146,7 @@ export default function Sponsors() {
                         <div className='text-xl text-pink-600 font-Lustria font-semibold lg:mb-2 my-3 lg:my-0 mx-auto lg:mx-0'>QUINCY</div>
                         <div className='text-base font-Quattrocento'>
                             Calling all pastry enthusiasts! The ultimate pastry competition awaits you! Show off your culinary skills and create mouthwatering masterpieces that will leave taste buds dancing.
-                            <button className='text-white font-bold ml-2' onClick={() => setShowModal3(true)}>Read More</button>
+                            <button className='text-white font-semibold ml-2' onClick={() => setShowModal3(true)}>Read More</button>
                             {showModal3 ? (
                                 <>
                                     <div
@@ -181,7 +168,7 @@ export default function Sponsors() {
                                                 </div>
                                                 {/*body*/}
                                                 <div className="relative p-6 flex-auto">
-                                                    <p className="my-4 text-slate-500 text-base leading-relaxed">
+                                                    <p className="my-4 text-slate-500 lg:text-base text-sm leading-relaxed">
                                                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus eveniet esse temporibus mollitia. Veniam illum eaque sit natus ab nisi sequi repudiandae vero adipisci, voluptatibus cum doloremque impedit tenetur! Alias!
                                                         Quia ducimus labore quasi cupiditate vel. Ipsa possimus, illo voluptate sequi inventore suscipit, delectus beatae et neque magni enim molestias rem consectetur maxime odio, saepe aut. Aut itaque laborum recusandae.
                                                         Rerum possimus laboriosam nisi, dolores, quibusdam sit dolore suscipit, ipsum voluptates sequi id et perferendis natus quasi. Numquam, autem, odit in iusto ipsa pariatur, explicabo iure ad dolorum nostrum ratione.
