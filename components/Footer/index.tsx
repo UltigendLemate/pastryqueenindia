161c41c71e455697a9e243d7dcd9af34f08288a4
RajKunx2 <113442064+RajKunx2@users.noreply.github.com>
2023-06-05 23:13:02 +0530

    font size

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="container mx-auto px-4">
         <div className="flex flex-wrap text-left lg:text-left">
           <div className="w-full lg:w-6/12 px-4">
-            <h1 className='special-font text-[3.7rem] text-4xl bg-black mb-6 text-primary'>PASTRY EVENT</h1>
+            <h1 className='special-font text-4xl md:text-6xl bg-black mb-6 text-primary'>PASTRY EVENT</h1>
             <h5 className="text-lg mt-0 mb-2 text-blueGray-600">
               Sweeten your senses, indulge in mastery!
               Join us for a delectable pastry showdown!
