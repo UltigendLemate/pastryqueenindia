72e708a0da6c2896fa9c58a02d55ab2879100c83
Chaitanya Anand <chaitanya.anand@gmail.com>
2024-05-06 00:04:27 +0530

    jpg->JPG

--- components/About.jsx
+++ components/About.jsx
@@ -62,26 +62,20 @@ export default function About() {
           hidden: { opacity: 0, y: 60 },
         }}>
           <div>
-          <Image ref={ref} width={600} height={600}
+          <img src="/images/DSC_8053.jpg" alt="" className='rounded-md max-h-96 object-cover hidden md:block' />
+
+        </div>
+        <div>
+        {/* ref={ref} width={600} height={600}
             initial="hidden"
             animate={controls}
             transition={{ duration: 1.5 }}
             variants={{
               visible: { opacity: 1, x: 0 },
               hidden: { opacity: 0, x: 100 },
-            }} src="/images/DSC_8053.jpg" alt="" className='rounded-md max-h-96 object-cover hidden md:block' />
+            }}  */}
 
-        </div>
-        <div>
-
-            <Image ref={ref} width={600} height={600}
-            initial="hidden"
-            animate={controls}
-            transition={{ duration: 1.5 }}
-            variants={{
-              visible: { opacity: 1, x: 0 },
-              hidden: { opacity: 0, x: -100 },
-            }} src="/images/DSC_7840.jpg" alt="" className='rounded-md max-h-96 object-cover hidden md:block' />
+            <img src="/images/DSC_7840.JPG" alt="" className='rounded-md max-h-96 object-cover hidden md:block' />
             </div>
 
         
