9c2276085dc206b1cea76d91ce24a16be1b45c41
Chaitanya Anand <chaitanya.anand@gmail.com>
2024-05-06 14:24:03 +0530

    indian-->india

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -63,7 +63,7 @@ const Footer = () => {
         <div className="flex flex-wrap items-center md:justify-between justify-center">
           <div className="w-full md:w-4/12 px-4 mx-auto text-center">
             <div className="text-sm text-blueGray-500 font-semibold py-1">
-              Copyright © <span id="get-current-year">Pastry Queen Indian Cup 2024</span>
+              Copyright © <span id="get-current-year">Pastry Queen India Cup 2024</span>
 
             </div>
           </div>
