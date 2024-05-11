8869e705ce1f27742e40b40d4c24c36ff39ea341
Chaitanya Anand <chaitanya.anand@gmail.com>
2024-05-12 02:50:48 +0530

    jpic linking added here

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import Sponsors from '@/components/Sponsors/Sponsors'
 import Organizers from '@/components/Organizers/index'
 import Participants from '@/components/Participants'
 import Jury from '@/components/Jury'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -26,6 +27,12 @@ export default function Home() {
       {/* <Participants /> */}
       {/* <div className='fixed w-10 h-10 top-0 left-0 bg-black'> </div> */}
       <Contact />
+      <div className='mx-auto mb-10 -mt-10'>
+        <h2 className='text-center text-2xl pb-5'>Previously Organized</h2>
+        <Link href="https://jrpastryindiancup.com/">
+        <img src="logo.jpg" alt="junior pstry event logo" className='aspect-square w-60' />
+        </Link>
+      </div>
     </Layout>
   )
 }
