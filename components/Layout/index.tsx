07c99f8511f3b7c77884fd682e94642785f33398
Chaitanya Anand <chaitanya.anand@gmail.com>
2025-02-11 17:04:12 +0530

    link

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Footer from "../Footer";
 import Navbar from "../Navbar";
 import Sponsors from "../Sponsors";
+import Link from "next/link";
 
 type Props = {
   children: React.ReactNode;
@@ -30,7 +31,9 @@ const Layout = (props: Props) => {
         <div className="popup fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-[999]">
           <div className="relative">
             <button onClick={handleClosePopup} className="close-button text-sm absolute top-1 font-bold right-1 w-7 h-7 bg-black aspect-square text-white rounded-full">X</button>
-            <img src="/banner.png" alt="Popup" className="popup-image sm:max-h-[600px] aspect-auto" />
+            <Link href="https://ita-aahar.webflow.io/ ">
+              <img src="/banner.png" alt="Popup" className="popup-image sm:max-h-[600px] aspect-auto" />
+            </Link>
           </div>
         </div>
       )}
