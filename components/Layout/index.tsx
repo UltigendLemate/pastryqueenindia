ad180268d070d7657f4a3cc357f7861da7d9cd44
Chaitanya Anand <chaitanya.anand@gmail.com>
2025-02-19 15:11:23 +0530

    pic change

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -27,16 +27,16 @@ const Layout = (props: Props) => {
     <div className={` ${showPopup ? 'h-screen overflow-hidden' : ''}`}>
       <Navbar />
       <div className={`  grid gap-14 justify-center items-center bg-white text-black ${props.className}`}>{props.children}</div>
-      {/* {showPopup && (
+      {showPopup && (
         <div className="popup fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-[999]">
           <div className="relative">
             <button onClick={handleClosePopup} className="close-button text-sm absolute top-1 font-bold right-1 w-7 h-7 bg-black aspect-square text-white rounded-full">X</button>
             <Link href="https://ita-aahar.webflow.io/ ">
-              <img src="/banner.png" alt="Popup" className="popup-image sm:max-h-[600px] aspect-auto" />
+              <img src="/bannerad.jpg" alt="Popup" className="popup-image sm:max-h-[600px] aspect-auto" />
             </Link>
           </div>
         </div>
-      )} */}
+      )}
       <Footer />
     </div>
 
